547d834d31760d9e0f20972ad8be152bddbdbadd
AhnQiraj <AhnQirajLiu@gmail.com>
2024-10-08 08:39:55 +0800

    fix external vue and element

--- packages/cpis-ui/vite.config.js
+++ packages/cpis-ui/vite.config.js
@@ -8,8 +8,13 @@ import UnoCSS from 'unocss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), UnoCSS()],
+
   build: {
+    rollupOptions: {
+      external: ['vue', 'element-ui'],
+    },
     lib: {
+
       name: 'cpis-ui',
       fileName: 'index',
       entry: resolve(__dirname, 'src/index.js')
