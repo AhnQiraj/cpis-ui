52f440e7873f731d5d96590af8138fccc522f955
AhnQiraj <AhnQirajLiu@gmail.com>
2024-12-05 10:31:33 +0800

    fix use primary

--- packages/cpis-ui/src/packages/container-header/src/container-header.vue
+++ packages/cpis-ui/src/packages/container-header/src/container-header.vue
@@ -4,7 +4,7 @@
       <slot />
     </template>
     <div v-else class="px-4 flex flex-row items-center h-[52px]">
-      <div class="bg-blue-6 w-1 h-[18px] line-height-none" />
+      <div class="bg-primary-6 w-1 h-[18px] line-height-none" />
       <div class="text-lg font-bold ml-2">{{ title }}</div>
     </div>
   </header>
