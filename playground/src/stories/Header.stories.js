87fbc7e7f5d45fa2eb5821e646d914d948a2480f
AhnQiraj <AhnQirajLiu@gmail.com>
2025-01-24 13:37:15 +0800

    fix container header

--- playground/src/stories/Header.stories.js
+++ playground/src/stories/Header.stories.js
@@ -27,6 +27,67 @@ export default {
     title: '这里是标题'
   }
 }
-export const Defaulta = {
-  name: '这里是标题'
+
+export const 标题 = {
+  name: 'title',
+  parameters: {
+    docs: {
+      source: {
+        code: `
+    <template>
+      <CpisContainerHeader title="基础信息" columns="1">
+      </CpisContainerHeader>
+    </template>
+`
+      }
+    }
+  },
+  render: (args, { argTypes }) => {
+    return {
+      props: Object.keys(argTypes),
+      components: {
+        CpisContainerHeader
+      },
+      template: `
+      <div>
+        <CpisContainerHeader title="标题" />
+      </div>
+      `
+    }
+  }
+}
+
+export const CustomRightSlot = {
+  name: '自定义右侧内容',
+  parameters: {
+    docs: {
+      source: {
+        code: `<template>
+  <CpisFormSection title="基础信息" columns="1">
+    <template #right>
+      右侧内容
+    </template>
+  </CpisFormSection>
+</template>
+`
+      }
+    }
+  },
+  render: (args, { argTypes }) => {
+    return {
+      props: Object.keys(argTypes),
+      components: {
+        CpisContainerHeader
+      },
+      template: `
+      <div>
+        <CpisContainerHeader title="这里是标题">
+          <template #right>
+            右侧内容
+          </template>
+        </CpisContainerHeader>
+      </div>
+      `
+    }
+  }
 }
