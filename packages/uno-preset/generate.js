87aa47ba35561b094f399c39ae3076a8f1f60a6c
AhnQiraj <AhnQirajLiu@gmail.com>
2025-02-21 11:25:20 +0800

    fix remove autpprettier

--- packages/uno-preset/generate.js
+++ packages/uno-preset/generate.js
@@ -1,33 +1,35 @@
 function generateColorCSSVars(color) {
-    return Object.entries(color)
-      .map(([key, value]) => {
-        return `--${key}: ${value};`
-      })
-      .filter(Boolean)
-      .join('\n')
+  return Object.entries(color)
+    .map(([key, value]) => {
+      return `--${key}: ${value};`
+    })
+    .filter(Boolean)
+    .join('\n')
+}
+
+function colorCSSVarsStyles(lightVars, darkVars, { themeName }) {
+  return `
+.theme-${themeName} {${lightVars}} \n
+.dark .theme-${themeName} {${darkVars}} \n
+`
+}
+
+export function generateCSSVars(themes) {
+  if (!Array.isArray(themes)) {
+    throw new Error(`themes must be an array`)
   }
-  
-  function colorCSSVarsStyles(lightVars, darkVars, { themeName }) {
-    return `
-    .theme-${themeName} {${lightVars}}
-    .dark .theme-${themeName} {${darkVars}}`
-  }
-  
-  export function generateCSSVars(themes) {
-    if (!Array.isArray(themes)) {
-      throw new Error(`themes must be an array`)
-    }
-    return themes.map(theme => {
+  return themes
+    .map(theme => {
       const { name = 'cpis-blue', cssVars = {} } = theme
       let cssStyle = ''
       const { light, dark } = cssVars
       const lightVars = generateColorCSSVars(light)
       const darkVars = generateColorCSSVars(dark)
-    
+
       cssStyle += colorCSSVarsStyles(lightVars, darkVars, {
         themeName: name
       })
       return cssStyle
-    }).join('\n')
-  }
-  
+    })
+    .join('\n')
+}
