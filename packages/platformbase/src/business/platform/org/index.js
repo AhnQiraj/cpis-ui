452002902dc23c92dbe118f9c468658f0884bbdb
AhnQiraj <AhnQirajLiu@gmail.com>
2025-02-21 15:08:05 +0800

    Delete packages/platformbase directory

--- packages/platformbase/src/business/platform/org/index.js
+++
@@ -1,5 +0,0 @@
-//import Vue from 'vue'
-//Vue.component('IbpsEmployeeSelector', () => import('./employee/selector.vue'))
-//Vue.component('IbpsOrgSelector', () => import('./org/selector.vue'))
-//Vue.component('IbpsPositionSelector', () => import('./position/selector.vue'))
-//Vue.component('IbpsRoleSelector', () => import('./role/selector.vue'))
