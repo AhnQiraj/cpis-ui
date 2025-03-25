8c88867309b1b361ceb820684035669711b52c73
AhnQiraj <AhnQirajLiu@gmail.com>
2025-03-25 17:40:16 +0800

    fix demo

---
+++ packages/cpis-ui/src/packages/demo/extend.vue
@@ -0,0 +1,13 @@
+<script>
+import { Tag } from 'element-ui'
+export default {
+  name: 'cpis-extend',
+  extends: Tag,
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
---
+++ packages/cpis-ui/src/packages/demo/forwarding.vue
@@ -0,0 +1,24 @@
+<template>
+  <ElTag class="cpis-tag" ref="elTag" v-on="$listeners" v-bind="$attrs">
+    <template v-for="(_, name) in $slots" #[name]="scopeData">
+      <slot :name="name" v-bind="scopeData"></slot>
+    </template>
+  </ElTag>
+</template>
+<script>
+import { Tag } from 'element-ui'
+export default {
+  name: 'CpisTag',
+  comments: {
+    ElTag: Tag
+  },
+  methods: {
+    // some method
+  }
+}
+</script>
+<style lang="scss">
+.cpis-tag {
+  color: red;
+}
+</style>
